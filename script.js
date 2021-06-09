073c4bfd027a694b58e340b149e92b78cd6b3ac8
Kevin Crawford <crawfordkevin101@gmail.com>
2021-06-08 21:37:48 -0400

    added localStorage function

--- script.js
+++ script.js
@@ -59,12 +59,19 @@ var myDay = [
 
 ]
 
-
-
 // Adds the current date and time to the header
 function headerDate() {
     var currentHeaderDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(currentHeaderDate);
 }
 headerDate();
+
+// Will store data into localStorage
+function saveReminder() {
+    localStorage.setItem("myDay", JSON.stringify(myDay));
+}
+
+function displayReminder() {
+    
+}
 
