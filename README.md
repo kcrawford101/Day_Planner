d038cd2dd6626a9c46115d973d823b3c6d95a030
Kevin Crawford <crawfordkevin101@gmail.com>
2021-06-09 21:08:38 -0400

    adjusted hours and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Day_Planner
+
+## About 
+This day planner is to help the user organize and plan their tasks for the day. 
+
+### Features 
+- Uses jQuery and moment.js libraries
+- Uses localStorage pseudo data
+
+## Screenshot
--- script.js
+++ script.js
@@ -2,60 +2,68 @@
 var myDay = [
     {
         id: "1",
+        hour: "9",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
         hour: "10",
         time: "10",
         meridiem: "am",
         reminder: ""
     },
     {
-        id: "2",
+        id: "3",
         hour: "11",
         time: "11",
-        meridiem: "am",
+        meridiem: "pm",
         reminder: ""
     },
     {
-        id: "3",
+        id: "4",
         hour: "12",
         time: "12",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: "4",
-        hour: "1",
-        time: "1",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
         id: "5",
-        hour: "2",
-        time: "2",
+        hour: "1",
+        time: "13",
         meridiem: "pm",
         reminder: ""
     },
     {
         id: "6",
-        hour: "3",
-        time: "3",
+        hour: "2",
+        time: "14",
         meridiem: "pm",
         reminder: ""
     },
     {
         id: "7",
-        hour: "4",
-        time: "4",
+        hour: "3",
+        time: "15",
         meridiem: "pm",
         reminder: ""
     },
     {
         id: "8",
-        hour: "5",
-        time: "5",
+        hour: "4",
+        time: "16",
         meridiem: "pm",
         reminder: ""
     },
+    {
+        id: "9",
+        hour: "5",
+        time: "17",
+        meridiem: "pm",
+        reminder: ""
+    },
+    
 
 ]
 
